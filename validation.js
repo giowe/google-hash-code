5a1c574639ea601c3cd36d2778982a50706d3fe6
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-01-22 17:40:10 +0100

    fix validation

--- validation.js
+++ validation.js
@@ -6,14 +6,21 @@ const test = ({n, t}) => {
     t()
     u.logSuccess(`Test "${n}" succeded`)
   } catch(e) {
-    u.logFail(`Test "${n}" failed: expected ${expect.expected} but got ${expect.actual}`)
+    u.logFail(`Test "${n}" failed. ${e}`)
   }
 }
 
-const tests = [
-  {n: "0 = 1", t: () => expect(1).toEqual(true)},
-  {n: "ciao = false", t: () => expect("ciao").toEqual(false)}
+const output = {
+  totalTurns: 1,
+  maxTurns: 10,
+  currentProducts: 10,
+  totalProducts: 5
+}
+
+const generateTests = (output) => [
+  { n: 'totalTurns < maxTurns', t: () => expect(output.totalTurns).toBeLessThan(output.maxTurns) },
+  { n: 'currentProducts < totalProducts', t: () => expect(output.currentProducts).toBeLessThan(output.totalProducts) }
 ]
 
 u.log("Begin tests...")
-tests.map(t => test(t))
+generateTests(output).map(t => test(t))
