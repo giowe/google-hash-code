76edabdae27e4d92eef2e2813924ba2d10a07232
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-01-22 19:47:36 +0100

    validation turni droni

--- validation.js
+++ validation.js
@@ -10,10 +10,52 @@ const test = ({n, t}) => {
   }
 }
 
-const generateTests = (parsedInput, output) => [
-  { n: 'totalTurns < maxTurns', t: () => expect(output.totalTurns).toBeLessThan(parsedInput.maxTurns) },
-  { n: 'currentProducts < totalProducts', t: () => expect(output.currentProducts).toBeLessThan(parsedInput.totalProducts) }
-]
+
+const _sampleOutput = {
+  droneCommands: [
+    [
+      { type: 'load', target: 0, productType: 0, amount: 1, turns: 2 },
+      { type: 'load', target: 0, productType: 1, amount: 1, turns: 3 },
+      { type: 'deliver', target: 0, productType: 0, amount: 1, turns: 2 },
+      { type: 'load', target: 1, productType: 2, amount: 1, turns: 3 },
+      { type: 'deliver', target: 0, productType: 2, amount: 1, turns: 1 },
+    ],
+    [
+      { type: 'load', target: 1, productType: 2, amount: 1, turns: 2 },
+      { type: 'deliver', target: 2, productType: 2, amount: 1, turns: 3 },
+      { type: 'load', target: 0, productType: 0, amount: 1, turns: 4 },
+      { type: 'deliver', target: 1, productType: 0, amount: 1, turns: 1 }
+    ]
+  ],
+  deliveredOrders: [
+    [ 1, 0, 1 ],
+    [ 1, 0, 0 ],
+    [ 0, 0, 1 ]
+  ]
+}
+
+//VALIDATION:
+//- Format of the output file matches description above
+//- All commands are valid
+//- No order receives more products of any type than the number of products of this type specified in the order
+//- All commands for any given drone take at most T turns in total, where T is the number of tuns of the simulation
+
+const generateTests = (parsedInput, output) => {
+
+  let tests = []
+
+  const getTurnsPerDrone = (drone) => {
+    let turns = drone.map(command => command.turns)
+    return turns.reduce( (acc, cur) => acc + cur, 0 )
+  } 
+
+  output.droneCommands.map((drone, i) => {
+    let turnsSum = getTurnsPerDrone(drone)
+    tests.push({ n: `Drone ${i} total turns (${turnsSum}) < max turns (${parsedInput.turns})`, t: () => expect(turnsSum).toBeLessThan(parsedInput.turns) })
+  })
+
+  return tests 
+}
 
 const runTests = (parsedInput, output) => {
   u.log("Begin tests...")
