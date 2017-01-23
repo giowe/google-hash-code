63ef7cb342b09f41bea0ed86aa496d083c928157
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-01-23 17:57:15 +0100

    sample fail

--- samples/output.js
+++ samples/output.js
@@ -8,8 +8,8 @@ const sampleOutput = {
       { type: 'deliver', target: 0, productType: 2, amount: 1, turns: 1 }
     ],
     [
-      { type: 'load', target: 1, productType: 2, amount: 1, turns: 2 },
-      { type: 'deliver', target: 2, productType: 2, amount: 1, turns: 3 },
+      { type: 'load', target: 1, productType: 2, amount: 1, turns: 20 },
+      { type: 'deliver', target: 2, productType: 2, amount: 1, turns: 30 },
       { type: 'load', target: 0, productType: 0, amount: 1, turns: 4 },
       { type: 'deliver', target: 1, productType: 0, amount: 1, turns: 1 }
     ]
