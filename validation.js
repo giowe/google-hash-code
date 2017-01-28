df8a795887855a61cb0e685db192d52241e0a414
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-01-28 21:29:01 +0100

    refactoring

--- validation.js
+++ validation.js
@@ -1,20 +1,28 @@
 const expect = require('expect')
 const u = require('./modules/utils')
 
-const test = (test) => {
+const runTest = (test) => {
   try {
-    test.get('test')()
+    test.get('function')()
     u.logSuccess(`Test "${test.get('text')}" succeded`)
   } catch(e) {
-    u.logFail(`Test "${map.get('text')}" failed. ${e}`)
+    u.logFail(`Test "${test.get('text')}" failed. ${e}`)
   }
 }
 
 const generateTests = (parsedInput, output) => {
-
+  
   let tests = new Set()
 
-// All commands for any given drone take at most T turns in total, where T is the number of tuns of the simulation
+  const createTest = (text, test) => {
+    let m = new Map()
+    m.set('text', text)
+    m.set('function', test)
+    return m
+  }
+
+
+  // All commands for any given drone take at most T turns in total, where T is the number of tuns of the simulation
   const getTurnsPerDrone = (drone) => {
     let turns = drone.map(command => command.turns)
     return turns.reduce( (acc, cur) => acc + cur, 0 )
@@ -22,26 +30,23 @@ const generateTests = (parsedInput, output) => {
 
   output.droneCommands.forEach( (drone, i) => {
     let turnsSum = getTurnsPerDrone(drone)
-    let m = new Map()
-    m.set('text', `Drone ${i} total turns (${turnsSum}) < max turns (${parsedInput.turns})`)
-    m.set('test', () => expect(turnsSum).toBeLessThanOrEqualTo(parsedInput.turns))
-    tests.add(m)
+    tests.add(createTest(`Drone ${i} total turns (${turnsSum}) < max turns (${parsedInput.turns})`, () => expect(turnsSum).toBeLessThanOrEqualTo(parsedInput.turns)))
   })
 
 
-// No order receives more products of any type than the number of products of this type specified in the order
+  // No order receives more products of any type than the number of products of this type specified in the order
 
   //output.droneCommands.forEach( (drone, i) => {
 
   //}
 
-// Return tests
+
   return tests
 }
 
 const runTests = (parsedInput, output) => {
   u.log("Begin tests...")
-  generateTests(parsedInput, output).forEach(t => test(t))
+  generateTests(parsedInput, output).forEach(test => runTest(test))
 }
 
 module.exports = { runTests }
