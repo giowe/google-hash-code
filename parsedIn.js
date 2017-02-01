a85bd9dfbcbad5c786c40a5409dc79e2a1817282
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-02-01 21:31:26 +0100

    mods

--- parsedIn.js
+++ parsedIn.js
@@ -1,70 +1,31 @@
 'use strict';
 const Parser = require('./modules/InParser');
-const p = new Parser();
+const p = new Parser({autoCast: false});
 const u = require('./modules/utils');
 
 const parsedInput = {
-  field: {
-    x: p.consumeCol(),
-    y: p.consumeCol()
-  },
-  dronesCount: p.consumeCol(),
-  turns: p.consumeCol(),
-  maxPayload: p.consumeCol(),
-  productTypes: {
-    count: p.consumeCol(),
-    weight: p.consumeRow()
-  },
-  warehousesCount: p.consumeCol('warehousesCount'),
-  warehouses: p.reiteratedStruct('warehousesCount', () => ({
-    x: p.consumeCol(), y: p.consumeCol(),
-    products: p.consumeRow(),
-  })),
-  ordersCount: p.consumeCol('ordersCount'),
-  orders: p.reiteratedStruct('ordersCount', () => ({
-    x: p.consumeCol(), y: p.consumeCol(),
-    productsCount: p.consumeCol(),
-    products: p.consumeRow()
-  }))
+  N: p.consumeCol('N'),
+  M: p.consumeCol('M'),
+  pic: [
+    p.reiteratedStruct('M', () => {
+
+    })
+  ]
 };
 
 const model = {
-  field: {
-    x: 100,
-    y: 100
-  },
-  dronesCount: 3,
-  turns: 50,
-  maxPayload: 500,
-  productTypes: {
-    count: 3,
-    weight: [100, 5, 450]
-  },
-  warehousesCount: 2,
-  warehouses: [{
-    x:0, y:0,
-    products: [5, 1, 0],
-  }, {
-    x:5, y:5,
-    products: [0, 10, 2],
-  }],
-  ordersCount: 3,
-  orders: [{
-    x:1, y:1,
-    productsCount: 2,
-    products: [2, 0]
-  }, {
-    x:3, y:3,
-    productsCount: 1,
-    products: [0]
-  },
-  {
-    x:5, y:6,
-    productsCount: 1,
-    products: [2]
-  }]
+  N: 5,
+  M: 7,
+  pic:
+  [
+    [0,0,0,0,1,0,0],
+    [0,0,1,1,1,0,0],
+    [0,0,1,0,1,0,0],
+    [0,0,1,1,1,0,0],
+    [0,0,1,0,0,0,0]
+  ],
 };
 
 //Parser.validateOverModel(model, parsedInput);
-//u.logJson(parsedInput);
+u.logJson(parsedInput);
 module.exports = parsedInput;
--- samples/output.js
+++ samples/output.js
@@ -1,3 +1,25 @@
-const sampleOutput = {};
+const sampleOutput = [{
+    name: 'PAINTSQ',
+    x: 3,
+    y: 3,
+    s: 1
+  },
+  {
+    name: 'PAINTSQ',
+    x: 0,
+    y: 4,
+    s: 0
+  },
+  {
+    name: 'PAINTSQ',
+    x: 5,
+    y: 2,
+    s: 0
+  },
+  {
+    name: 'ERASECELL',
+    x: 3,
+    y: 3
+  }];
 
 module.exports = sampleOutput;
