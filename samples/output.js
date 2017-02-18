b9e1429b2de646bcccbf720a5a042b58663a4b2e
Federico Cabassi <federico.cabassi@gmail.com>
2017-02-18 16:11:51 +0100

    aggiunto out file

--- samples/output.js
+++ samples/output.js
@@ -1,3 +1,7 @@
-const sampleOutput = [];
+const sampleOutput = [
+	{ r1: 0, c1: 0, r2: 2, c2: 1 },
+	{ r1: 0, c1: 2, r2: 2, c2: 2 },
+	{ r1: 0, c1: 3, r2: 2, c2: 4 },
+];
 
 module.exports = sampleOutput;
