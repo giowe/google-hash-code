de6f0a9b8d5a92704a5a21f26144c7ce04fa4d1a
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-02-18 16:26:40 +0100

    parsdIn

--- parsedIn.js
+++ parsedIn.js
@@ -5,8 +5,22 @@ const sampleInput = require('./samples/input');
 const p = new Parser({autoCast: false, colSeparator: ''} );
 const u = require('./modules/utils');
 
-const parsedInput = {}; //TODO!!!
+const firstRowData = p.consumeRow().join('').split(' ');
+const R = firstRowData[0];
+const C = firstRowData[1];
+const L = firstRowData[2];
+const H = firstRowData[3];
+const parsedInput = {
+  R, C, L, H,
+  pizza: p.reiteratedStruct(Number(R), (l) => {
+    const out = [];
+    for (let i = 0; i < C; i++) {
+      out.push(p.consumeCol());
+    }
+    return out;
+  })
+};
 
 //Parser.validateOverModel(sampleInput, parsedInput);
-//u.logJson(parsedInput);
+console.log(parsedInput);
 module.exports = parsedInput;
