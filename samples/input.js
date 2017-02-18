38cfc2ebf05779ef6c09fe4bcffe4c484e5e5d32
Federico Cabassi <federico.cabassi@gmail.com>
2017-02-18 16:09:17 +0100

    exampleIn

--- samples/input.js
+++ samples/input.js
@@ -1,5 +1,13 @@
 const sampleInput = {
-
+	R: 3,
+	C: 5,
+	L: 1,
+	H: 6,
+	pizza: [
+		['T', 'T', 'T', 'T', 'T'],
+		['T', 'M', 'M', 'M', 'T'],
+		['T', 'T', 'T', 'T', 'T']
+	]
 };
 
 module.exports = sampleInput;
