2a4896c80d93835f3bf75dbc9b20146e44ea798d
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-02-18 17:06:33 +0100

    mods

--- main.js
+++ main.js
@@ -14,6 +14,21 @@ const {
   R, C, L, H, pizza
 } = initialState;
 
+const minSlices = Math.floor(R*C/H);
+const toppingsCount = {
+  T: 0,
+  M: 0
+};
+
+pizza.forEach((row) => {
+  row.forEach((topping) => toppingsCount[topping]++ );
+});
+
+const minorTopping = toppingsCount.T < toppingsCount.M ? 'T' : 'M';
+const maxSlices = Math.floor(toppingsCount[minorTopping] / L);
+
+console.log(minSlices, maxSlices);
+
 //eseguire il programma scrivendo -v per avviare la validation
 if (argv.v || argv.validation) {
   validation.runTests(initialState, out);
@@ -24,3 +39,10 @@ u.logColor('green', finalScore);
 outParser.produceOutput(finalScore, out);
 
 module.exports = out;
+
+//**************************** HELPER FUNCTIONS ****************************
+function sliceArea(r1, c1, r2, c2) {
+  const r = 1 + r2 - r1;
+  const c = 1 + c2 - c1;
+  return r*c;
+}
