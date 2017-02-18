f4fa5f00902e6bc6dc06c50ba2c8bca8347bb4a8
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-02-18 19:11:56 +0100

    finita validation

--- validation.js
+++ validation.js
@@ -1,49 +1,61 @@
-'use strict';
+'use strict'
 
-const expect = require('expect');
-const u = require('./modules/utils');
+const expect = require('expect')
+const u = require('./modules/utils')
 
-const createTest = (text, test) => new Map().set('text', text).set('function', test);
+const createTest = (text, test) => new Map().set('text', text).set('function', test)
 
 const runTest = (test) => {
   try {
-    test.get('function')();
+    test.get('function')()
     u.logSuccess(`Test "${test.get('text')}" succeded`)
   } catch(e) {
     u.logFail(`Test "${test.get('text')}" failed. ${e}`)
   }
-};
+}
 
 const generateTests = (input, output) => {
 
-  const tests = new Set();
+  const tests = new Set()
 
   // Each cell of the pizza must be included in at most one slice
+  const getCell = (x, y) => ({x: x, y: y})
+  const isCellInSlice = (cell, slice) => (cell.x >= slice.c1 && cell.x <= slice.c2) && (cell.y >= slice.r1 && cell.y <= slice.r2) ? true : false
+  const getCellArrayWithCountPerSlice = () => {
+    let cellArray = []
+    input.pizza.forEach((row, rowIndex) => {
+      cellArray.push([])
+      row.forEach((_, columnIndex) => {
+        cellArray[rowIndex].push({x: columnIndex, y: rowIndex, count: 0})
+        output.forEach(slice => {
+          if (isCellInSlice(getCell(columnIndex, rowIndex), slice)) cellArray[rowIndex][columnIndex].count += 1
+        })
+      })
+    })
+    return cellArray
+  }
+
+  getCellArrayWithCountPerSlice().forEach(column => {
+    column.forEach(cell => {
+      const text = `Expect cell count per slice (${cell.count}) <= 1`
+      const test = () => expect(cell.count).toBeLessThanOrEqualTo(1)
+      tests.add(createTest(text, test))
+    })
+  })
+
 
   // Each cell must contain at least L cells of each ingredient type
   const countTopping = (ingredientsArray, type) => ingredientsArray.reduce(((count, topping) => topping === type ? count + 1 : count ), 0)
-  const isCellInSlice = (cell, slice) => (cell.x >= slice.c1 && cell.x <= slice.c2) && (cell.y >= slice.r1 && cell.y <= slice.r2) ? true : false
 
   const getSlice = (slice) => {
     let currentSlice = []
     input.pizza.forEach((row, rowIndex) => {
       row.forEach((topping, columnIndex) => {
-        if (isCellInSlice({x: columnIndex, y: rowIndex}, slice)) currentSlice.push(topping)
+        if (isCellInSlice(getCell(columnIndex, rowIndex), slice)) currentSlice.push(topping)
       })
     })
     return currentSlice
   }
-  //  const getCellTopping = (x, y) => {
-  //  input.pizza.forEach((row, rowIndex) => {
-  //    row.forEach((topping, columnIndex) => {
-  //      const cellCoordinates = {x: columnIndex, y: rowIndex}
-  //      if (cellCoordinates.x === x && cellCoordinates.y === y) {
-  //        console.log('cell', cellCoordinates)
-  //        console.log('topping', topping)
-  //      }
-  //    })
-  //  })
-  //}
 
   output.forEach((curSlice, i) => {
     const ingredientsArray = getSlice(curSlice)
@@ -55,9 +67,6 @@ const generateTests = (input, output) => {
     const mushText = `Expect mushroom count for slice ${i} (${curSliceMushCount})>= L (${input.L})`
     const mushTest = () => expect(curSliceMushCount).toBeGreaterThanOrEqualTo(input.L)
     tests.add(createTest(mushText, mushTest)) 
-  }) 
-
-  output.forEach((curSlice, i) => {
   }) 
 
 
@@ -72,19 +81,19 @@ const generateTests = (input, output) => {
 
   // Return
   return tests
-};
+}
 
 const runTests = (input, output) => {
-  u.log('Generating tests...');
-  console.time('\nTotal running time');
-  console.time('Tests generated in');
-  const tests = generateTests(input, output);
-  console.timeEnd('Tests generated in');
-  console.log('\nRunning tests...');
-  console.time('Tests completed in');
-  tests.forEach(test => runTest(test));
-  console.timeEnd('Tests completed in');
-  console.timeEnd('\nTotal running time');
-};
+  u.log('Generating tests...')
+  console.time('\nTotal running time')
+  console.time('Tests generated in')
+  const tests = generateTests(input, output)
+  console.timeEnd('Tests generated in')
+  console.log('\nRunning tests...')
+  console.time('Tests completed in')
+  tests.forEach(test => runTest(test))
+  console.timeEnd('Tests completed in')
+  console.timeEnd('\nTotal running time')
+}
 
-module.exports = { runTests };
+module.exports = { runTests }
