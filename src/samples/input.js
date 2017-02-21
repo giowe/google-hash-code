60347437fe2287baeb6ddd8e6c879ba1db1719d4
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-02-21 21:21:33 +0100

    in e out

--- src/samples/input.js
+++ src/samples/input.js
@@ -1,5 +1,31 @@
 const sampleInput = {
-
+  R: 2,
+  S: 5,
+  U: 1,
+  P: 2,
+  M: 5,
+  uSlots: [
+    [0, 0] //per numero di U
+  ],
+  servers: [
+    //per numero di M
+    {
+      size: 3,
+      capacity: 10
+    },{
+      size: 3,
+      capacity: 10
+    },{
+      size: 2,
+      capacity: 5
+    },{
+      size: 1,
+      capacity: 5
+    },{
+      size: 1,
+      capacity: 1
+    },
+  ]
 };
 
 module.exports = sampleInput;
