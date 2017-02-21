d6b43efc04bced94b5c688c4ca40dfe47021057c
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-02-21 21:24:31 +0100

    outParser finito

--- src/outParser.js
+++ src/outParser.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 const produceOutput = (filename, output) => {
   const logger = fs.createWriteStream(filename)
-  const outArray = output.map(row => row === 'x' ? 'x' : `${row.x} ${row.y} ${row.pool}`)
+  const outArray = output.map(row => row === 'x' ? 'x\n' : `${row.x} ${row.y} ${row.pool}\n`)
   outArray.forEach(line => {
     logger.write(line.toString())
   })
