9ccc1c2304dd49611876041e51743cd8b5e4115d
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-02-22 00:31:24 +0100

    fixato scorer

--- src/scorer.js
+++ src/scorer.js
@@ -3,11 +3,52 @@ const h = require('./helpers');
 const u = require('./modules/utils');
 
 module.exports = (parsedIn, out) => {
-  const minPoolCap = [];
-  const l = parsedIn.P;
-  for (let i = 0; i < l; i++) {
-     minPoolCap.push(h.getPoolMinCap(h.getServersInPool(i, out), parsedIn.servers));
+
+  const availableServers = out.filter(server => server !== 'x')
+
+  //const totalCapacityForPool = (pool) => {
+  //  pool.reduce()
+  //}
+
+  const getPoolMinCapacity = (poolIndex) => {
+    let maxCapacity = 0
+    availableServers.forEach((server, serverIndex) => {
+      if (server.pool === poolIndex) {
+        const serverCapacity = parsedIn.servers[serverIndex].capacity
+        maxCapacity += serverCapacity
+      }
+    })
+    const rows = parsedIn.R
+    let capacityToSubtract = 0
+    for (let i = 0; i < rows; i++) {
+      let poolRowCapacity = 0
+      availableServers.forEach((server, serverIndex) => {
+        if (server.pool === poolIndex && server.row === i) {
+          const serverCapacity = parsedIn.servers[serverIndex].capacity
+          poolRowCapacity += serverCapacity
+        }
+      })
+      if (poolRowCapacity >= capacityToSubtract) capacityToSubtract = poolRowCapacity
+    } 
+    return maxCapacity - capacityToSubtract
   }
 
-  return Math.min(...minPoolCap);
+  let realScore = 0
+
+  let poolsLength = parsedIn.P
+  for (let i = 0; i < poolsLength; i++) {
+    if (i === 0) realScore = getPoolMinCapacity(i)
+    else if (getPoolMinCapacity(i) < realScore) realScore = getPoolMinCapacity(i)
+  }
+
+  return realScore
+
+
+  //const minPoolCap = [];
+  //const l = h.getPoolsCount(out);
+  //for (let i = 0; i < l; i++) {
+  //   minPoolCap.push(h.getPoolMinCap(h.getServersInPool(i, out), parsedIn.servers));
+  //}
+
+  //return Math.min(...minPoolCap);
 };
