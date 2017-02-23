8efb126dd4c7fa22d33e52a2ad8a87fdab2ca907
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-02-23 19:27:16 +0100

    outparser

--- src/outParser.js
+++ src/outParser.js
@@ -5,8 +5,6 @@ const fs = require('fs')
 const produceOutput = (filename, output) => {
   const logger = fs.createWriteStream(filename)
 
-  console.log(output)
-
   const outArray = []
     .concat(output.length + '\n')
     .concat(output.map(cache => (cache.cacheId + ' ' + cache.videos.map(video => video + ' ') + '\n').replace(',', '')))
@@ -15,8 +13,6 @@ const produceOutput = (filename, output) => {
     logger.write(line.toString())
   })
   logger.end()
-
-  console.log(outArray)
 
   return outArray.join('')
 }
