9d97834963be66ab941ec54480e2a69a2c7d1d8d
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2019-02-17 15:07:27 +0100

    refactoring in corso

--- src/runmulti.js
+++ src/runmulti.js
@@ -2,4 +2,6 @@ require("shelljs/global")
 
 const command = `node . ${process.argv.slice(2).join(" ")} --s3`
 console.log("EXECUTING", command)
-while (true) exec(command)
+while (true) {
+  exec(command)
+}
