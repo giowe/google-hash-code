f40c13ec6b23807691e723a60d302a3688cdca2a
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2019-02-17 17:07:00 +0100

    mods

--- src/outParser.js
+++ src/outParser.js
@@ -1,7 +1,18 @@
 const { writeFileSync } = require("fs")
 
 const produceOutput = (filename, output) => {
+  const out = output.reduce((acc, rides) => {
+    acc += rides.length + " "
+    rides.forEach(({ rideId }) => {
+      acc += rideId + " "
+    })
+    acc += "\n"
 
+    return acc
+  }, "")
+
+  console.log(out)
+  return out
 }
 
 module.exports = { produceOutput }
