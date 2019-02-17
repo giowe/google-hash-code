7e1f5a2b4014a45d399a8b56dece6e5c644bf54e
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2019-02-17 17:03:13 +0100

    mods

--- src/outParser.js
+++ src/outParser.js
@@ -1,17 +1,7 @@
-const {  createWriteStream } = require("fs")
+const { writeFileSync } = require("fs")
 
 const produceOutput = (filename, output) => {
-  const logger = createWriteStream(filename)
 
-  const outArray = []
-    .concat(`${output.length}\n`)
-    .concat(output.map(cache => (cache.cacheId + " " + cache.videos.join(" ") + "\n")))
-
-  outArray.forEach(line => {
-    logger.write(line.toString())//.replace(new RegExp(/,/g), ''))
-  })
-  logger.end()
-  return outArray.join("")
 }
 
 module.exports = { produceOutput }
