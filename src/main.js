fa67edf732c7b79503dd1a1c86d5add25d492e43
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2019-02-23 16:44:42 +0100

    mods

--- src/main.js
+++ src/main.js
@@ -1,21 +1,18 @@
 const { spawn } = require("child_process")
-const { V, s3, _, mock, p } = require("simple-argv")
+const { s3, _, mock, p } = require("simple-argv")
 const fs = require("fs")
 const path = require("path")
 const {
   log, logJson, logFail, logSuccess, logColor,
   getOutputFilesFolder, getInputFilesFolder,
-  getDistance
 } = require("./modules/utils")
-const m = require("mathjs")
-const h = require("./helpers")
+
 const { uploadScore } = require("./modules/state")
 const initialState = mock ? require("./samples/input.js") : require("./parsedIn")
 const sampleOut = require("./samples/output")
 
 const scorer = require("./scorer")
 const outParser = require("./outParser")
-const jsgraphs = require("js-graph-algorithms")
 const out = mock ? require("./samples/output.js") : []
 
 //**************************** CALL EXTERNAL PROCESS ****************************
@@ -32,30 +29,24 @@ proc.stderr.on("data", data => {
 })
 
 proc.on("close", () => {
-  const tempFile = require(tempFilePath)
-  log(tempFile)
+  const out = require(tempFilePath)
+  const inputFolderPath = getInputFilesFolder()
+  const files = fs.readdirSync(inputFolderPath)
+  const inputFileName = files[_[0]] ? path.parse(files[_[0]]).name : "test"
+  const outFolderPath = getOutputFilesFolder(inputFileName)
+  const finalScore = scorer(initialState.parsedIn, out)
+  const filename = `${finalScore.padStart(10, "0")}.out`
+
+  log(filename)
+  try { fs.mkdirSync("./outFiles") } catch(ignore) {  }
+  try { fs.mkdirSync(outFolderPath) } catch(ignore) {  }
+
+  const filenameWithPath = path.join(outFolderPath, filename)
+
+  logSuccess(`\nScore: ${finalScore}`)
+
+  const output = outParser.produceOutput(filenameWithPath, out)
+  if (s3) {
+    uploadScore(filename, output)
+  }
 })
-
-//*******************************************************************************
-//
-// const inputFolderPath = getInputFilesFolder()
-// const files = fs.readdirSync(inputFolderPath)
-// const inputFileName = files[_[0]] ? path.parse(files[_[0]]).name : "test"
-// const outFolderPath = getOutputFilesFolder(inputFileName)
-// const finalScore = scorer(initialState.parsedIn, out)
-// const filename = `${finalScore.padStart(10, "0")}.out`
-//
-// log(filename)
-// try { fs.mkdirSync("./outFiles") } catch(ignore) {  }
-// try { fs.mkdirSync(outFolderPath) } catch(ignore) {  }
-//
-// const filenameWithPath = path.join(outFolderPath, filename)
-//
-// logSuccess(`\nScore: ${finalScore}`)
-//
-// const output = outParser.produceOutput(filenameWithPath, out)
-// if (s3) {
-//   uploadScore(filename, output)
-// }
-
-module.exports = out
