c84f76fa0e4080dccef5cf1d3765883914289313
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2019-02-23 17:14:46 +0100

    mods

--- src/process/process.py
+++ src/process/process.py
@@ -5,6 +5,8 @@ import json
 dir_path = os.path.dirname(os.path.realpath(__file__))
 (process_path, input_path, output_path) = sys.argv
 
+out = []
+
 with open(input_path) as f:
   data = json.load(f)
 
@@ -12,4 +14,4 @@ print("input:")
 print(json.dumps(data, indent=4))
 
 with open(output_path, "w") as f:
-  json.dump({"a": "b"}, f, indent=4)
+  json.dump(out, f, indent=4)
