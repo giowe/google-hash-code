c462421cfdda918c5090000c65c52033419495c9
Valerio Versace <mail@valerioversace.it>
2019-02-23 16:42:55 +0100

    reading parsed in from sample python process

--- src/process/process.py
+++ src/process/process.py
@@ -5,5 +5,11 @@ import json
 dir_path = os.path.dirname(os.path.realpath(__file__))
 (process_path, input_path, output_path) = sys.argv
 
+with open(input_path) as f:
+  data = json.load(f)
+
+print("input:")
+print(json.dumps(data, indent=4))
+
 with open(output_path, "w") as f:
-  json.dump({"a": "b"}, f)
+  json.dump({"a": "b"}, f, indent=4)
