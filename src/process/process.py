214224887b54c8ede8a6bc57a34368f984cc4f48
Valerio Versace <mail@valerioversace.it>
2019-02-28 21:21:37 +0100

    fix

--- src/process/process.py
+++ src/process/process.py
@@ -28,7 +28,7 @@ hList = initialState["hList"]
 def generate_tags_structure():
     pickle_path = PurePath(dir_path).parent.parent.joinpath("parsedInFiles").joinpath("tags_structure_{}.pickle".format(str(PurePath(input_path).stem)))
 
-    if os.path.isfile(pickle_path):
+    if os.path.isfile(str(pickle_path)):
         with open(pickle_path, 'rb') as f:
             tags_structure = pickle.load(f)
     else:
