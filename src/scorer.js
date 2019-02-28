886a6a1315db0a3053ac7f2474c9958d8cae570b
Valerio Versace <mail@valerioversace.it>
2019-02-28 20:02:26 +0100

    scorer, first validation (checking if photo is parsed twice)

--- src/scorer.js
+++ src/scorer.js
@@ -1,14 +1,33 @@
 module.exports = (parsedIn, out) => {
+  const { photos } = parsedIn
+
   let score = 0
 
-  const assignedRides = []
+  const parsedPhotos = []
 
-  out.forEach(slide => {
+  for (let i = 1; i < out.length; i++) {
+    A = out[i-1]
+    B = out[i]
 
-    slide.forEach(photo => {
+    const tagsA = []
+    const tagsB = []
 
-    })
-  })
+    for (let photo of A) {
+      tagsA.push(...photos[photo].tags)
+    }
+
+    parsedPhotos.push(...A)
+
+    for (let photo of B) {
+      if (parsedPhotos.includes(photo)) throw `trying to include photo ${photo} twice`
+      tagsB.push(...photos[photo].tags)
+    }
+
+    const commonTags = tagsA.filter(value => tagsB.includes(value))
+
+    score = score + Math.min(tagsA.length, tagsB.length, commonTags.length)
+
+  }
 
   return score.toString()
 }
