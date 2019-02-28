14aaeb3af52d06d9d58cab90f2a6ac0618bea6aa
Valerio Versace <mail@valerioversace.it>
2019-02-28 20:21:12 +0100

    validation fix, added check for number of photos per slide

--- src/scorer.js
+++ src/scorer.js
@@ -9,24 +9,29 @@ module.exports = (parsedIn, out) => {
     A = out[i-1]
     B = out[i]
 
+    if (A.length !== 1 && A.length !== 2) throw `slide ${i-1} has ${A.length} photos but photos must be 1 or 2`
+    if (i === out.length-1 && B.length !== 1 && B.length !== 2) throw `slide ${i} has ${A.length} photos but photos must be 1 or 2`
+
     const tagsA = []
     const tagsB = []
 
     for (let photo of A) {
+      if (parsedPhotos.includes(photo)) throw `trying to include photo ${photo} twice`
+      parsedPhotos.push(photo)
       tagsA.push(...photos[photo].tags)
     }
 
-    parsedPhotos.push(...A)
-
     for (let photo of B) {
-      if (parsedPhotos.includes(photo)) throw `trying to include photo ${photo} twice`
       tagsB.push(...photos[photo].tags)
     }
 
     const commonTags = tagsA.filter(value => tagsB.includes(value))
 
     score = score + Math.min(tagsA.length, tagsB.length, commonTags.length)
+  }
 
+  for (let photo of out[out.length-1]) {
+    if (parsedPhotos.includes(photo)) throw `trying to include photo ${photo} twice`
   }
 
   return score.toString()
