d5bb7dd3f765994950fdf3de0b8dd531bbc89815
Valerio Versace <mail@valerioversace.it>
2019-03-15 17:57:59 +0100

    added sample output

--- src/samples/output.js
+++ src/samples/output.js
@@ -1,7 +1,9 @@
 const sampleOutput = [
-  [0],
-  [3],
-  [1, 2]
+  { "o": [2, 5], "hq": [15, 1], "p": "URRRRRUUURRRRDRRRRU" }, // o = office, hq = headquarter, p = path
+  { "o": [2, 5], "hq": [14, 6], "p": "URRRRRUUURRRRDRRRDDDD" },
+  { "o": [2, 5], "hq": [3, 8], "p": "DDDR" },
+  { "o": [16, 7], "hq": [14, 6], "p": "LLU" },
+  { "o": [16, 7], "hq": [17, 9], "p": "DDR" }
 ]
 
 module.exports = sampleOutput
