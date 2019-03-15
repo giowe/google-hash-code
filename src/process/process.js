e2453a9c8fa0713efbc6f4f4ab1d0b6e2102a877
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2019-03-15 18:16:08 +0100

    mods

--- src/process/process.js
+++ src/process/process.js
@@ -5,17 +5,42 @@ const {
   getDistance
 } = require("../modules/utils")
 
+const random = require("random")
 const { writeFileSync } = require("fs")
-const jsgraphs = require("js-graph-algorithms")
+const EasyStar = require("easystarjs")
+const easystar = new EasyStar.js()
 
-const {} = require(infile)
+const { N, M, C, R, CO, W_MAP } = require(infile)
 const out = []
 
-//**************************** PROCESS HELPERS ****************************
 
+const zeroUnoGrid = W_MAP.map(e => e.map(e => e === null ? 0 : 1))
+easystar.setGrid(W_MAP)
 
-//**************************** PROCESS OPERATIONS ****************************
+const tiles = [
+  800, 200, 150, 120, 100, 70, 50
+]
+easystar.setAcceptableTiles(tiles)
+tiles.forEach(e => easystar.setTileCost(e, e))
 
-//logJson(out)
+const remainingCO = CO.slice()
+
+for (let r = 0; r < R; r++) {
+  let y
+  let x
+  do {
+    x = random.int(0, N - 1)
+    y = random.int(0, M - 1)
+  } while (zeroUnoGrid[y][x] !== 1 || CO.some(({ x: coX, y: coY }) => x === coX && y === coY))
+
+  const ratio = Math.ceil(C / R)
+  const i = random.int(0, remainingCO.length - 1)
+  const co = remainingCO.pop(i)
+
+  easystar.findPath(x, y, co.x, co.y, (data) => {
+    console.log("ciao", data)
+  })
+}
+easystar.calculate()
 
 writeFileSync(outfile, JSON.stringify(out))
