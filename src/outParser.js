fc8dbd5d72cc3da8d66861fdde42198e7d7d2892
Valerio Versace <mail@valerioversace.it>
2019-03-15 19:16:06 +0100

    scorer base senza bonus

--- src/outParser.js
+++ src/outParser.js
@@ -1,15 +1,12 @@
 const { writeFileSync } = require("fs")
 
 const produceOutput = (filename, output) => {
-  console.log(output)
-
   let buffer = ""
 
   for (const hq of output) {
     buffer = buffer + hq.o[0] + " " + hq.o[1] + " " + hq.p + "\n"
   }
 
-  console.log(buffer)
   writeFileSync(filename, buffer)
   return buffer
 }
--- src/scorer.js
+++ src/scorer.js
@@ -1,64 +1,44 @@
 module.exports = (parsedIn, out) => {
+
+  function lookupCost(tile, move) {
+    let cost = null
+    let nextTile = null
+    switch(move) {
+      case "U":
+        nextTile = [tile[0], tile[1]-1]
+        break
+      case "R":
+        nextTile = [tile[0]+1, tile[1]]
+        break
+      case "D":
+        nextTile = [tile[0], tile[1]+1]
+        break
+      case "L":
+        nextTile = [tile[0]-1, tile[1]]
+        break
+    }
+    cost = parsedIn.W_MAP[nextTile[1]][nextTile[0]]
+    if (cost !== null) {
+      return [cost, nextTile]
+    } else {
+      throw "moving to non walkable tile"
+    }
+  }
+
   let score = 0
 
-  /*
-  const { photos } = parsedIn
+  for (const hq of out) {
+    const rewardMax = parsedIn.CO.find(customer => hq.hq[0] === customer.x && hq.hq[1] === customer.y).points
+
+    const costTotal = hq.p.split("").reduce((acc, cur) => {
+      return lookupCost(acc[1], cur)
+    }, [0, [hq.o[0], hq.o[1]]])
 
 
-  if (out.length < 2) {
-    console.log("Warning: output has only 1 slide")
-    return score.toString()
+    const reward = rewardMax - costTotal[0]
+
+    score = score + reward
   }
-
-  const parsedPhotos = []
-
-  for (let i = 1; i < out.length; i++) {
-    A = out[i-1]
-    B = out[i]
-
-    if (A.length !== 1 && A.length !== 2) throw `slide ${i-1} has ${A.length} photos but photos must be 1 or 2`
-    if (i === out.length-1 && B.length !== 1 && B.length !== 2) throw `slide ${i} has ${A.length} photos but photos must be 1 or 2`
-
-    if (A.length === 1) {
-      if (parsedIn.photos[A[0]].orientation !== "H") throw `photo ${A} is the single photo in its slide but it's not horizontal`
-    }
-    if (A.length === 2) {
-      if (parsedIn.photos[A[0]].orientation !== "V" || parsedIn.photos[A[1]].orientation !== "V") throw `photos ${A} are together in their slide but one or both of them are not vertical`
-    }
-
-    const tagsA = []
-    const tagsB = []
-
-    for (let photo of A) {
-      if (parsedPhotos.includes(photo)) throw `trying to include photo ${photo} twice`
-      parsedPhotos.push(photo)
-      tagsA.push(...photos[photo].tags)
-    }
-
-    for (let photo of B) {
-      tagsB.push(...photos[photo].tags)
-    }
-
-    const commonTags = tagsA.filter(value => tagsB.includes(value))
-
-    score = score + Math.min(tagsA.length, tagsB.length, commonTags.length)
-  }
-
-  for (let photo of out[out.length-1]) {
-    if (parsedPhotos.includes(photo)) throw `trying to include photo ${photo} twice`
-    parsedPhotos.push(photo)
-  }
-
-  if (parsedIn.N !== parsedPhotos.length) console.log(`Warning: input includes ${parsedIn.N} photos but ${parsedPhotos.length} were used`)
-  if (parsedIn.N < parsedPhotos.length) throw `${parsedPhotos.length} photos were used but input includes ${parsedIn.N} photos`
-
-  if (B.length === 1) {
-      if (parsedIn.photos[B[0]].orientation !== "H") throw `photo ${B} is the single photo in its slide but it's not horizontal`
-  }
-  if (B.length === 2) {
-      if (parsedIn.photos[B[0]].orientation !== "V" || parsedIn.photos[B[1]].orientation !== "V") throw `photos ${B} are together in their slide but one or both of them are not vertical`
-  }
-  */
 
   return score.toString()
 }
